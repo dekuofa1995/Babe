990fae0a2f72137b2b404a48c5750210ab4d08b7
dekuofa1995 <dekuofa1995@gmail.com>
2020-01-09 11:38:07 +0800

    :wrench: update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   semi: false,
   singleQuote: true,
-  printWidth: 120, // 行宽 120
+  printWidth: 90, // 行宽 90
   tabWidth: 2,
   trailingComma: 'all', // 尾部逗号
+  arrowParens: 'always',
 }
